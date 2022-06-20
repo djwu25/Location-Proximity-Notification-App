674c15241e23c40f8d7183d3e71d8615c2c33a5e
Duke Wu <d3wu@ucsd.edu>
2022-06-20 12:37:39 -0700

    Initial Commit

---
+++ Location Proximity Notification App/Location_Proximity_Notification_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Location_Proximity_Notification_AppApp.swift
+//  Location Proximity Notification App
+//
+//  Created by Duke Wu on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Location_Proximity_Notification_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
